ff7a591cde505d85ecd58a1bd8a75cfe4dd96207
kendrickweb94 <kendrickzgeorge@gmail.com>
2024-12-28 00:14:47 -0800

    added real time map

--- src/routes/Support.jsx
+++ src/routes/Support.jsx
@@ -80,7 +80,17 @@ const Support = () => {
           <br />
           <br />
           <div className="max-w-6xl w-full">
-            <img src={SupportMap} alt="" />
+            <div className="w-full rounded-md">
+              <iframe
+                width="100%"
+                height="400"
+                frameborder="0"
+
+                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=eneka%20rupokwu%20port%20harcourt+(The%20future)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              >
+                <a href="https://www.gps.ie/">gps devices</a>
+              </iframe>
+            </div>
           </div>
           <br />
           <br />
